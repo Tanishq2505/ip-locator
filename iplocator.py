4d25e9bea8084ff648b7122e193f6432986bc930
Tanishq Kashyap <tanishqa89@gmail.com>
2021-04-03 10:58:50 +0530

    locator working, print needed

--- iplocator.py
+++ iplocator.py
@@ -1,1 +1,20 @@
 #This is a python code to locate anyone's address using IP Address
+import urllib.request
+import os
+import json
+import geopy.geocoders
+
+while True:
+    ip = input('Enter The IP Adress: ')
+    url = "http://ip-api.com/json/"
+    with urllib.request.urlopen(url + ip) as response:
+        data = response.read()
+    values = json.loads(data)
+
+    print(values)
+    lat = values['lat']
+    lon = values['lon']
+    geolocator = geopy.Nominatim(user_agent="reverse_locator")
+    address = geolocator.reverse(f'{lat},{lon}')
+    print(address)
+    break
