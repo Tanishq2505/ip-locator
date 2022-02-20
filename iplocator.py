d510cfe46b400da15dbb30b7bef3a20b34dbf97d
Tanishq Kashyap <tanishqa89@gmail.com>
2022-02-20 22:29:56 +0530

    loop removed

--- iplocator.py
+++ iplocator.py
@@ -4,17 +4,16 @@ import os
 import json
 import geopy.geocoders
 
-while True:
-    ip = input('Enter The IP Adress: ')
-    url = "http://ip-api.com/json/"
-    with urllib.request.urlopen(url + ip) as response:
-        data = response.read()
-    values = json.loads(data)
 
-    print(values)
-    lat = values['lat']
-    lon = values['lon']
-    geolocator = geopy.Nominatim(user_agent="reverse_locator")
-    address = geolocator.reverse(f'{lat},{lon}')
-    print(address)
-    break
+ip = input('Enter The IP Adress: ')
+url = "http://ip-api.com/json/"
+with urllib.request.urlopen(url + ip) as response:
+    data = response.read()
+values = json.loads(data)
+
+print(values)
+lat = values['lat']
+lon = values['lon']
+geolocator = geopy.Nominatim(user_agent="reverse_locator")
+address = geolocator.reverse(f'{lat},{lon}')
+print(address)
